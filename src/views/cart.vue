cb5c6b538b095021ace4513c49b8e326a318ad57
unknown <Wonder.D.Wang@yzw.corp>
2017-08-16 16:56:48 +0800

    fix

--- src/views/cart.vue
+++ src/views/cart.vue
@@ -1,21 +1,38 @@
 <template>
     <app-header :header-data="headerData"></app-header>
     <div class="cart-wrapper">
-        <div class="sub-title">cart<span class="cart-num">（0）</span></div>
+        <div class="sub-title">cart<span class="cart-num">（{{cartNum}}）</span></div>
+        <div></div>
+        <div class="group">
+        	<span class="minus" @click="minus()">-</span>
+        	<input class="num" v-model="cartNum" />
+        	<span class="plus" @click="plus()">+</span>
+        </div>
     </div>
 </template>
 <script type="text/babel">
-    require("less/cart.less")
-    export default{
-        data(){
-            return{
-                headerData:{
-                    pageTitle:"cart",
-                    showBack:true,
-                    showRight:true,
-                    showTab:false
-                }
-            }
+require("less/cart.less")
+export default{
+    data(){
+        return{
+            headerData:{
+                pageTitle:"cart",
+                showBack:true,
+                showRight:true,
+                showTab:false
+            },
+            cartNum:0
         }
-    }
+    },
+    methods:{
+    minus(){
+		if(this.cartNum>0){
+			this.cartNum--;
+		}
+	},
+	plus(){
+		this.cartNum++;
+	}
+  }
+}
 </script>
